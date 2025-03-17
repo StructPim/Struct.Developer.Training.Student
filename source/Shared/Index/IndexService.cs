0c188afe18cc4b8ca8f303055cee4495d4bed725
Simon Schandorff <schandorff@struct.dkj>
2025-03-17 11:39:08 +0100

    Added pagination to product viewer
    (cherry picked from commit e889319a16dd7326fc9ba43e24d6e1a1d4c0c3a7)

--- source/Shared/Index/IndexService.cs
+++ source/Shared/Index/IndexService.cs
@@ -60,8 +60,9 @@ namespace Shared.Index
 
 
         #region lookup for product portal
-        public List<ProductIndexModel> GetProducts(LookupRequest lookupModel)
+        public List<ProductIndexModel> GetProducts(LookupRequest lookupModel, out int total)
         {
+            total = 0;
             //read entire json file into memory
             var index = GetFullIndex<ProductIndexModel>(IndexType.ProductIndex, lookupModel.CultureCode);
 
@@ -80,6 +81,9 @@ namespace Shared.Index
             {
                 documents = documents.Where(x => x.Name.Contains(lookupModel.SearchQuery) || x.Description.Contains(lookupModel.SearchQuery)).ToList();
             }
+
+            //set total
+            total = documents.Count();
 
             //paginate the results
             documents = documents.Take(lookupModel.PageSize).ToList();
--- source/Website/Models/ProductsListViewModel.cs
+++ source/Website/Models/ProductsListViewModel.cs
@@ -3,5 +3,19 @@
     public class ProductsListViewModel
     {
         public List<ListProductViewModel> Products { get; set; } = new List<ListProductViewModel>();
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; } = 24;
+        public int TotalResults { get; set; }
+        public int TotalPages
+        {
+            get
+            {
+                if (TotalResults == 0)
+                {
+                    return 0;
+                }
+                return TotalResults / PageSize;
+            }
+        }
     }
 }
